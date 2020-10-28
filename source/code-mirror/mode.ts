c83c840ddb06823d204716a60c5e2be3382e12d4
Ziphil <yfe23457@nifty.com>
2020-10-28 18:37:55 +0900

    後方参照をシンタックスハイライトできるようモード定義を変更

--- source/code-mirror/mode.ts
+++ source/code-mirror/mode.ts
@@ -9,6 +9,7 @@ let CodeMirrorAny = CodeMirror as any;
 CodeMirrorAny.defineSimpleMode("zatlin", {
   start: [
     {regex: /"([^"\\]|\\"|\\\\|\\u[A-Fa-f0-9]{0,4})*?"/, token: "string"},
+    {regex: /\&\d+/, token: "operator"},
     {regex: /\d+\.?\d*|\.\d+/, token: "number"},
     {regex: /[a-zA-Z][a-zA-Z0-9_]*/, token: "variable-1"},
     {regex: /\^/, token: "operator"},
