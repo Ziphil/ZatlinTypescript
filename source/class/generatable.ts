b95bdee4d450a775f52a88c10d71f149e4b4dba8
Ziphil <yfe23457@nifty.com>
2023-02-11 14:04:20 +0900

    コメントの体裁を変更

--- source/class/generatable.ts
+++ source/class/generatable.ts
@@ -10,9 +10,9 @@ export abstract class Generatable<C = void, D = void> {
 
   public abstract generate(zatlin: Zatlin, context: C): string;
 
-  // ちょうど from で与えられた位置から右向きにマッチするかどうかを調べます。
-  // マッチした場合はマッチした範囲の右端のインデックス (範囲にそのインデックス自体は含まない) の配列を返します。
-  // マッチしなかった場合は空配列を返します。
+  /** ちょうど `from` で与えられた位置から右向きにマッチするかどうかを調べます。
+   * マッチした場合はマッチした範囲の右端のインデックス (範囲にそのインデックス自体は含まない) の配列を返します。
+   * マッチしなかった場合は空配列を返します。*/
   public abstract match(string: string, from: number, zatlin: Zatlin, context: D): Array<number>;
 
   public test(string: string, zatlin: Zatlin, context: D): boolean {
@@ -25,16 +25,16 @@ export abstract class Generatable<C = void, D = void> {
     return false;
   }
 
-  // このオブジェクトがマッチ可能 (除外設定に置いて良い) ならば true を返し、そうでなければ false を返します。
+  /** このオブジェクトがマッチ可能 (除外設定に置いて良い) ならば `true` を返し、そうでなければ `false` を返します。*/
   public abstract isMatchable(zatlin: Zatlin): boolean;
 
   public abstract isValid(zatlin: Zatlin): boolean;
 
-  // 存在しない識別子を含んでいればそれを返し、そうでなければ undefined を返します。
+  /** 存在しない識別子を含んでいればそれを返し、そうでなければ `undefined` を返します。*/
   public abstract findUnknownIdentifier(zatlin: Zatlin): Identifier | undefined;
 
-  // 識別子定義文を全て展開したときに identifiers に含まれる識別子が含まれていればそれを返し、そうでなければ undefined を返します。
-  // 識別子の定義が循環参照していないかを調べるのに用いられます。
+  /** 識別子定義文を全て展開したときに `identifiers` に含まれる識別子が含まれていればそれを返し、そうでなければ `undefined` を返します。
+   * 識別子の定義が循環参照していないかを調べるのに用いられます。*/
   public abstract findCircularIdentifier(identifiers: Array<Identifier>, zatlin: Zatlin): Identifier | undefined;
 
 }
